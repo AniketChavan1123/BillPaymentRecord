0e0a51b325596c4a344f143bac42b88b05e6aaa8
Aniket Chavan <87638342+AniketChavan1123@users.noreply.github.com>
2021-08-04 18:37:25 +0530

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,29 @@
+var app = angular.module('myapp', []);
+app.controller('CartForm', function ($scope) {
+        $scope.cart = {
+            items: [{qty: '', description: '', cost: ''}]
+        }
+
+        $scope.addItem = function () {
+            $scope.cart.items.push({qty: '', description: '', cost: ''});
+        }
+
+        $scope.resetAll=function(){
+            angular.forEach($scope.cart.items, function (index){
+                $scope.cart.items.splice(index, 1);
+            });
+        }
+
+        $scope.removeItem = function (index) {
+            $scope.cart.items.splice(index, 1);
+        }
+
+        $scope.total = function () {
+            var total = 0;
+            angular.forEach($scope.cart.items, function (item) {
+                total += item.qty * item.cost;
+            })
+            return total;
+        }
+    }
+);
